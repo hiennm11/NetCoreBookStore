d47177b12fb9ca89cee6ed435f46459101ceebd0
Minh Hien <minhhiena712@gmail.com>
2020-07-29 15:42:05 +0700

    add book repo + basic ui

---
+++ NetCoreBookStore.Core/Repositories/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreBookStore.Data.EF;
+using NetCoreBookStore.Data.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreBookStore.Core.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<BookVM>> GetAllBook();
+    }
+
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public BookRepository(AppDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<BookVM>> GetAllBook()
+        {
+            return await _dbContext.Books.Select(x => new BookVM
+            {
+                Id = x.Id,
+                Description = x.Description,
+                Title = x.Title,
+                NameAlias = x.NameAlias,
+                Price = x.Price
+            }).ToListAsync();
+        }
+    }
+}
---
+++ NetCoreBookStore.Data/ViewModel/BookVM.cs
@@ -0,0 +1,16 @@
+﻿using NetCoreBookStore.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBookStore.Data.ViewModel
+{
+    public class BookVM
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }      
+        public double Price { get; set; }
+        public string NameAlias { get; set; }
+    }
+}
