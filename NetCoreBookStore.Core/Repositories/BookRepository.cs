d178db4c01228ebfb34a617943a1124a480e1cfe
Minh Hien <minhhiena712@gmail.com>
2020-07-29 16:06:07 +0700

    fisrt run with data

--- NetCoreBookStore.Core/Repositories/BookRepository.cs
+++ NetCoreBookStore.Core/Repositories/BookRepository.cs
@@ -11,7 +11,9 @@ namespace NetCoreBookStore.Core.Repositories
 {
     public interface IBookRepository
     {
-        Task<IEnumerable<BookVM>> GetAllBook();
+        Task<IEnumerable<BookVM>> GetAllBookAsync();
+        Task<IEnumerable<BookVM>> GetBookPagingAsync(int page, int pageSize);
+
     }
 
     public class BookRepository : IBookRepository
@@ -23,7 +25,7 @@ namespace NetCoreBookStore.Core.Repositories
             this._dbContext = dbContext;
         }
 
-        public async Task<IEnumerable<BookVM>> GetAllBook()
+        public async Task<IEnumerable<BookVM>> GetAllBookAsync()
         {
             return await _dbContext.Books.Select(x => new BookVM
             {
@@ -33,6 +35,18 @@ namespace NetCoreBookStore.Core.Repositories
                 NameAlias = x.NameAlias,
                 Price = x.Price
             }).ToListAsync();
+        }
+
+        public async Task<IEnumerable<BookVM>> GetBookPagingAsync(int page, int pageSize)
+        {
+            return await _dbContext.Books.Select(x => new BookVM
+            {
+                Id = x.Id,
+                Description = x.Description,
+                Title = x.Title,
+                NameAlias = x.NameAlias,
+                Price = x.Price
+            }).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
         }
     }
 }
---
+++ NetCoreBookStore/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreBookStore.Core.Repositories;
+
+namespace NetCoreBookStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public ShopController(IBookRepository bookRepository)
+        {
+            this._bookRepository = bookRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _bookRepository.GetAllBookAsync();
+            return View(response);
+        }
+    }
+}
