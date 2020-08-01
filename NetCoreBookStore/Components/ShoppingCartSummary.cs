906c86ac77e5bf842ef08dbc984e7911d2adfa01
Minh Hien <minhhiena712@gmail.com>
2020-08-01 09:32:40 +0700

    refactoring

--- NetCoreBookStore/Components/ShoppingCartSummary.cs
+++ NetCoreBookStore/Components/ShoppingCartSummary.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetCoreBookStore.Core.Repositories;
 using NetCoreBookStore.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NetCoreBookStore.Components
 {
