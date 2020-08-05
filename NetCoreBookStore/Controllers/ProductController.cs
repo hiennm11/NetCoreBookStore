ef111fbc59dac18bbb15b2a9a58de6d2bd2328e9
Minh Hien <minhhiena712@gmail.com>
2020-08-06 06:25:46 +0700

    add product list load more button

--- NetCoreBookStore/Controllers/ProductController.cs
+++ NetCoreBookStore/Controllers/ProductController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreBookStore.Core.Repositories;
+using NetCoreBookStore.Extensions;
 
 namespace NetCoreBookStore.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IBookRepository _bookRepository;
+        private const int _recordsPerLoad = 10;
 
         public ProductController(IBookRepository bookRepository)
         {
@@ -18,8 +20,9 @@ namespace NetCoreBookStore.Controllers
 
         public async Task<IActionResult> List()
         {
-            var response = await _bookRepository.GetAllAsync();
+            ViewBag.IsEndOfRecords = false;
             ViewBag.Recommended = await _bookRepository.GetRecomendedBooksAsync();
+            var response = await _bookRepository.GetPagingAsync(1, _recordsPerLoad);
             return View(response);
         }
 
@@ -28,6 +31,24 @@ namespace NetCoreBookStore.Controllers
             var response = await _bookRepository.GetSingleAsync(id);
             ViewBag.Recommended = await _bookRepository.GetRecomendedBooksAsync();
             return View(response);
+        }
+
+        public async Task<IActionResult> LoadMore(int? page)
+        {
+            page = page ?? 1;
+            ViewBag.IsEndOfRecords = false;
+            if (CoreExtension.IsAjaxRequest(Request))
+            {
+                var items = await _bookRepository.GetPagingAsync(page.Value, _recordsPerLoad);
+                ViewBag.IsEndOfRecords = ((page.Value * _recordsPerLoad) >= _bookRepository.TotalRow());
+                return PartialView("_BookItemsDiv", items);
+            }
+            else
+            {
+                ViewBag.Recommended = await _bookRepository.GetRecomendedBooksAsync();
+                var response = await _bookRepository.GetPagingAsync(page.Value, _recordsPerLoad);
+                return View("List", response);
+            }
         }
     }
 }
